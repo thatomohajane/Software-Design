3a43d8ce05838e0e06dc5622eeba9647b396b9de
Yabiso Wiseman Molefe <molefe.wiseman@gmail.com>
2024-04-20 08:39:22 +0200

    Server app code skeleton with routes

---
+++ server-app/routes/reports.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+// TODO: Do your end-point APIs here { GET, POST, PUT, DELETE}
+/* E.g GET route
+
+router.get('/api/reports', async (req, res) => {
+  try {
+    // Try something
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'An error occurred while fetching data' });
+  }
+}); */
+
+
+module.exports = router;
--- server-app/server.js
+++ server-app/server.js
@@ -1,12 +1,38 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3001;
 
 
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );      
-app.use(bodyParser.urlencoded({  extended: true }));
- 
+app.use(cors({
+    origin: 'http://localhost:3000', // Vue.js client's URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    credentials: true, // Allow cookies to be sent with requests
+}));
 
-var server = app.listen(3000, function () {
-   console.log("REST API listening on http://127.0.0.1:3000/");
-})
+// Adding routes to our  server
+const userManagementRoutes = require('./routes/userManagement'); 
+const issueManagementRoutes = require('./routes/issueManagement'); 
+const reportsRoutes = require('./routes/reports'); 
+const noticesRoutes = require('./routes/notices'); 
+const finesRoutes = require('./routes/fines'); 
+const loginRoutes = require('./routes/login'); 
+
+
+app.use(bodyParser.json());
+app.use(userManagementRoutes);
+app.use(issueManagementRoutes);
+app.use(reportsRoutes);
+app.use(noticesRoutes);
+app.use(finesRoutes);
+app.use(loginRoutes);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+module.exports = app;
